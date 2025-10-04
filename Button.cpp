fc67d71e58cce7cb19a19250d7f1e09c78513025
Bread Board Maniax <bbmaniax@users.noreply.github.com>
2025-10-04 10:47:55 +0900

    refactor: a bit

--- Button.cpp
+++ Button.cpp
@@ -1,29 +1,19 @@
-/*
-  Button Input Library
-
-  A library for handling button inputs with debouncing
-  and click detection.
-*/
-
-#include "Button.h"
+// Button
 
 #include <Arduino.h>
 
-Button::Button(uint8_t pin, unsigned long debounceDelay) : pin(pin), debounceDelay(debounceDelay), currentState(false), lastState(false), lastReading(false), lastDebounceTime(0) {}
+#include "Button.h"
 
-bool Button::isPressed() {
-  return currentState;
-}
-
-bool Button::isClicked() {
-  return lastState && !currentState;
-}
+Button::Button(uint8_t pin, unsigned long debounceDelay, unsigned long longPressDelay) : pin(pin), debounceDelay(debounceDelay), longPressDelay(longPressDelay), currentState(false), lastState(false), lastReading(false), lastDebounceTime(0), pressStartTime(0), longPressTriggered(false) {}
 
 void Button::begin() {
   pinMode(pin, INPUT_PULLUP);
   currentState = false;
   lastState = currentState;
   lastReading = currentState;
+  lastDebounceTime = millis();
+  pressStartTime = 0;
+  longPressTriggered = false;
 }
 
 void Button::update() {
@@ -32,6 +22,32 @@ void Button::update() {
   if ((millis() - lastDebounceTime) > debounceDelay) {
     lastState = currentState;
     currentState = reading;
+
+    if (!lastState && currentState) {
+      pressStartTime = millis();
+      longPressTriggered = false;
+    }
+
+    if (lastState && !currentState) {
+      if (!longPressTriggered && (millis() - pressStartTime) >= longPressDelay) {
+        longPressTriggered = true;
+      } else {
+        longPressTriggered = false;
+      }
+      pressStartTime = 0;
+    }
   }
   lastReading = reading;
+}
+
+bool Button::isPressed() {
+  return currentState;
+}
+
+bool Button::isClicked() {
+  return lastState && !currentState && !longPressTriggered;
+}
+
+bool Button::isLongPressed() {
+  return lastState && !currentState && longPressTriggered;
 }
